9c69f85699cb77275f0efb4ac6f208417f81ff8d
Robin Roeland <rroeland03@gmail.com>
2023-11-27 18:07:30 +0100

    DB toegevoegd

--- Data/KanbanDbContext.cs
+++ Data/KanbanDbContext.cs
@@ -1,10 +1,25 @@
 using Kanban_RMR.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Kanban_RMR.Data
 {
     public class KanbanDbContext : DbContext
     {
-    public DbSet<Ticket> Tickets { get; set; }      
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("KanbanDB");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
